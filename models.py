4cc73a7dc73cc2072b667800f5fd42c5df214cd0
Hamilkar247 <adonnibal96@gmail.com>
2021-07-29 21:01:02 +0200

    [ISS-1] zabawa z pymongo
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- models.py
+++ models.py
@@ -5,23 +5,34 @@ from typing import Optional
 
 client = MongoClient()
 db = client.test
+print(client.list_database_names())
+dblist = client.list_database_names()
+if "mydatabase" in dblist:
+    print("The database exists.")
+#col = db["blog_posts"]
+#print(db.list_collection_names())
+#print(col.drop())
+#print(db.list_collection_names())
 
-class PyObjectId(ObjectId):
 
+class PydanticObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
     def validate(cls, v):
-        raise ValueError('Invalid objectid')
-    
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type='string')
 
+
 class User(BaseModel):
-    _id: Optional[PyObjectId] = Field(alias='_id')
+    _id: Optional[PydanticObjectId] = Field(alias='_id')
     name: str
     username: str
     email: str
@@ -31,3 +42,20 @@ class User(BaseModel):
         json_encoders = {
             ObjectId: str
         }
+
+
+class Blog_post(BaseModel):
+    _id: Optional[PydanticObjectId] = Field(alias='_id')
+    title: str
+    author: str
+    content: str #Text
+    #created_at: datetime = datetime.now()
+    #published_at: datetime
+    #published: Optional[bool] = False
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+
--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-from models import db, User
+from models import db, User, Blog_post
 
 app = FastAPI()
 
-@app.get('/users')
-async def list_users():
-    users = []
-    for user in db.users.find():
-        users.append(User(**user))
-    return {'users': users}
 
-@app.post('/users')
+@app.post('/user/')
 async def create_user(user: User):
     if hasattr(user, 'id'):
         delattr(user, 'id')
@@ -18,6 +12,84 @@ async def create_user(user: User):
     user.id = ret.inserted_id
     return {'user': user}
 
+
+@app.get('/user/')
+async def get_users():
+    users = []
+    for user in db.users.find():
+        users.append(User(**user))
+    return {'users': users}
+
+
+@app.get('/user/{user_id}/')
+async def get_user():
+    # print(user_id)
+    # user = db.users.find_one({"_id": user_id})
+    # return {'user': user}
+    users = []
+    for user in db.users.find():
+        users.append(User(**user))
+    return {'users': users}
+
+
+@app.get('/user/{username}/')
+async def get_user(username: str):
+    user = db.users.find_one({"username": username})
+    return {'user': user}
+
+
+@app.post('/blog_posts/')
+async def create_blog_post(blog_post: Blog_post):
+    if hasattr(blog_post, 'id'):
+        delattr(blog_post, 'id')
+    ret = db.blog_posts.insert_one(blog_post.dict(by_alias=True))
+    blog_post.id = ret.inserted_id
+    return {"blog_post": blog_post}
+
+
+@app.get('/blog_posts/')
+async def get_blog_posts():
+    blog_posts = []
+    for blog_post in db.blog_posts.find_one():
+        blog_posts.append(Blog_post(**blog_post))
+    return {'blog_posts': blog_posts}
+
+
+@app.get('/blog_posts/{zachod_title}/')
+async def get_blog_post(zachod_title: str):
+    blog_posts = []
+    for blog_post in db.blog_posts.find_one({"title": zachod_title}):
+        blog_posts.append(Blog_post(**blog_post))
+    return {'blog_posts': blog_posts}
+
+
+#    blog_post = db.blog_posts.find_one({"id": blog_post_id})
+#    return {"blog_post:": blog_post}
+
+
+# @app.get('/user/{id}/')
+# async def put_user():
+
+# @app.get('/user/{id}/')
+# async def delete_user():
+
+# @app.get("/blog")
+# async def get_posts():
+#    posts = []
+#    for post in db.posts.find():
+#        posts.append(User(**user))
+#    return {'users': users}
+#
+#
+# @app.get('/blog')
+# async def create_post(post: Post):
+#    if hasattr(post, 'id'):
+#        delattr(post, 'id')
+#    ret = db.posts.insert_one(post.dict(by_alias=True))
+#    post.id = ret.inserted_id
+#    return {'user': user}
+
+
 @app.get('/')
-async def index():
-    return {'hello': 'world'}
+async def read_root():
+    return {'home': 'Home page'}
