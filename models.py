d403cda15af06f994b7f5eb1296476e0935124ff
Hamilkar247 <adonnibal96@gmail.com>
2021-08-05 12:46:32 +0200

    [#1] jakieś dziwne próby
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- models.py
+++ models.py
@@ -14,13 +14,25 @@ from typing import Optional
 #print(db.list_collection_names())
 #print(col.drop())
 #print(db.list_collection_names())
+import generateData
+from mongo_models import wektor_danych
 
 client = MongoClient("mongodb://localhost:27017")
 db_miernik = client["api_miernik"]
 print(client.list_database_names())
 dblist = client.list_database_names()
 if "api_miernik" in dblist:
-    print("The database exist")
+    print("baza danych już istnieje")
+wektor_probek_col = db_miernik["wektory_probek"]
+wektor_probek = [generateData.generataVector()]
+dane_probkowe = {
+     "temperatura": wektor_probek.__getitem__(0)[0],
+     "pm2_5": wektor_probek.__getitem__(0)[1],
+     "pm5": wektor_probek.__getitem__(0)[2],
+     "pm10": wektor_probek.__getitem__(0)[3],
+     "hydroetyl": wektor_probek.__getitem__(0)[4],
+     "tlen": wektor_probek.__getitem__(0)[5]
+}
 
 
 class PydanticObjectId(ObjectId):
@@ -39,13 +51,14 @@ class PydanticObjectId(ObjectId):
         field_schema.update(type='string')
 
 
-class Probka:
-    temperatura: int
-    pm2_5: int
-    pm5: int
-    pm10: int
-    hydroetyl: int
-    tlen: int
+class Wektor_Probek:
+    _id: Optional[PydanticObjectId] = Field(alias="_id")
+    temperatura: str
+    pm2_5: str
+    pm5: str
+    pm10: str
+    hydroetyl: str
+    tlen: str
 
     class Config:
         arbitrary_types_allowed = True
@@ -81,7 +94,7 @@ class User(BaseModel):
     name: str
     username: str
     email: str
-    
+
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {
--- run.py
+++ run.py
@@ -1,33 +1,121 @@
 from fastapi import FastAPI
-from models import db, User, Blog_post
-from mongo_models
+
+import generateData
+from models import db_miernik, dblist, User, Wektor_Probek  # , User, Blog_post, dblist
 
 app = FastAPI()
 
-@app.get('/pomiar/')
+sesja_dane = {
+    "data": "10.10.2021",
+    "start": "True",
+    "koniec": "False"
+}
+sesja = db_miernik["sesja"]
+#print(sesja.drop())
+#sesje_lista = []
+#x = sesja.insert_one(sesja_dane)
+#print(x.inserted_id)
+#y = {"": ""}
+#for x in sesja.find():
+#    print(x)
+#    sesje_lista.append(x)
+
+
+#wektor_probek = db_miernik["wektor_probek"]
+#wektor_probek.drop()
+#wektor_danych = []
+#wektor_danych.append(generateData.generataVector())
+#dane_pomiarowe = {
+#    "temperatura": wektor_danych.__getitem__(0)[0],
+#    "pm2_5": wektor_danych.__getitem__(0)[1],
+#    "pm5": wektor_danych.__getitem__(0)[2],
+#    "pm10": wektor_danych.__getitem__(0)[3],
+#    "hydroetyl": wektor_danych.__getitem__(0)[4],
+#    "tlen": wektor_danych.__getitem__(0)[5]
+#}
+#x = wektor_probek.insert_one(dane_pomiarowe)
+#for x in wektor_probek.find():
+#    print(x)
+
+
+# probki = []
+# if "api_miernik" in dblist:
+#    print("The database exist")
+# if True:
+#    pomiar = db_miernik["pomiar"]
+# wektor_danych = []
+# wektor_danych.append(generateData.generataVector())
+# dane_pomiarowe = {
+#    "temperatura": wektor_danych.__getitem__(-1)[0],
+#    "pm1_5": wektor_danych.__getitem__(0)[1],
+#    "pm4": wektor_danych.__getitem__(0)[2],
+#    "pm9": wektor_danych.__getitem__(0)[3],
+#    "hydroetyl": wektor_danych.__getitem__(-1)[4],
+#    "tlen": wektor_danych.__getitem__(-1)[5]
+# }
+# pomiar.insert_one(dane_pomiarowe)
+# for x in pomiar.find():
+#    print(x)
+#    probki.append(x)
+# return {"pomiary": probki}
+
+@app.get('/wektor_probek/')
 async def get_pomiar():
-    probki = []
+    wektory_probek = []
+    for wektor_probek in wektory_probek.find():
+        print(wektor_probek)
+        wektor_probek.append(Wektor_Probek(**wektor_probek))
+    return {"wektory_probek": wektory_probek}
 
 
-#@app.post('/user/')
-#async def create_user(user: User):
-#    if hasattr(user, 'id'):
-#        delattr(user, 'id')
-#    ret = db.users.insert_one(user.dict(by_alias=True))
-#    user.id = ret.inserted_id
-#    return {'user': user}
+@app.post('/wektor_probek/')
+async def create_user(wektor_probek: Wektor_Probek):
+    if hasattr(wektor_probek, 'id'):
+        delattr(wektor_probek, 'id')
+    ret = db_miernik.wektor_probek.insert_one(wektor_probek.dict(by_alias=True))
+    wektor_probek.id = ret.inserted_id
+    return {'wektor_probek': wektor_probek}
+
+
+@app.get("/start_sesja/")
+async def start_sesja():
+    sesja_dane = {
+        "data": "10.10.2021",
+        "start": "True",
+        "koniec": "False"
+    }
+    sesja = db_miernik["sesja"]
+    sesje_lista = []
+    x = sesja.insert_one(sesja_dane)
+    print(x.inserted_id)
+    y = {"": ""}
+    for x in sesja.find():
+        print(x)
+        sesje_lista.append(x)
+    return {'sesja': sesje_lista}
+
+
+@app.post('/user/')
+async def create_user(user: User):
+    if hasattr(user, 'id'):
+        delattr(user, 'id')
+    ret = db_miernik.users.insert_one(user.dict(by_alias=True))
+    user.id = ret.inserted_id
+    return {'user': user}
+
+
+@app.get('/user/')
+async def get_users():
+    users = []
+    for user in db_miernik.users.find():
+        users.append(User(**user))
+    return {'users': users}
+
+
 #
 #
-#@app.get('/user/')
-#async def get_users():
-#    users = []
-#    for user in db.users.find():
-#        users.append(User(**user))
-#    return {'users': users}
-#
-#
-#@app.get('/user/{user_id}/')
-#async def get_user():
+# @app.get('/user/{user_id}/')
+# async def get_user():
 #    # print(user_id)
 #    # user = db.users.find_one({"_id": user_id})
 #    # return {'user': user}
@@ -37,14 +125,14 @@ async def get_pomiar():
 #    return {'users': users}
 #
 #
-#@app.get('/user/{username}/')
-#async def get_user(username: str):
+# @app.get('/user/{username}/')
+# async def get_user(username: str):
 #    user = db.users.find_one({"username": username})
 #    return {'user': user}
 #
 #
-#@app.post('/blog_posts/')
-#async def create_blog_post(blog_post: Blog_post):
+# @app.post('/blog_posts/')
+# async def create_blog_post(blog_post: Blog_post):
 #    if hasattr(blog_post, 'id'):
 #        delattr(blog_post, 'id')
 #    ret = db.blog_posts.insert_one(blog_post.dict(by_alias=True))
@@ -52,16 +140,16 @@ async def get_pomiar():
 #    return {"blog_post": blog_post}
 #
 #
-#@app.get('/blog_posts/')
-#async def get_blog_posts():
+# @app.get('/blog_posts/')
+# async def get_blog_posts():
 #    blog_posts = []
 #    for blog_post in db.blog_posts.find_one():
 #        blog_posts.append(Blog_post(**blog_post))
 #    return {'blog_posts': blog_posts}
 #
 #
-#@app.get('/blog_posts/{zachod_title}/')
-#async def get_blog_post(zachod_title: str):
+# @app.get('/blog_posts/{zachod_title}/')
+# async def get_blog_post(zachod_title: str):
 #    blog_posts = []
 #    for blog_post in db.blog_posts.find_one({"title": zachod_title}):
 #        blog_posts.append(Blog_post(**blog_post))
